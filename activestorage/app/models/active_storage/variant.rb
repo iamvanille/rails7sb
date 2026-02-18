b8a3dee8a85c05747ed5d8d2567364fb819ba43c
iamvanille <vanille.paitier-debray@mail.mcgill.ca>
2026-02-18 13:39:07 -0600

    more trials with variant.rb specification

--- activestorage/app/models/active_storage/variant.rb
+++ activestorage/app/models/active_storage/variant.rb
@@ -129,20 +129,20 @@ class ActiveStorage::Variant
     end
 
     # only in rails 6 so try uncommenting if variant errors
-    # def specification
-    #   @specification ||=
-    #     if !blob.image? || ActiveStorage.web_image_content_types.include?(blob.content_type)
-    #       Specification.new \
-    #         filename: blob.filename,
-    #         content_type: "audio/wav",
-    #         format: nil
-    #     else
-    #       Specification.new \
-    #         filename: ActiveStorage::Filename.new("#{blob.filename.base}.wav"),
-    #         content_type: "audio/wav",
-    #         format: "wav"
-    #     end
-    # end
+    def specification
+      @specification ||=
+        if !blob.image? || ActiveStorage.web_image_content_types.include?(blob.content_type)
+          Specification.new \
+            filename: blob.filename,
+            content_type: "audio/wav",
+            format: nil
+        else
+          Specification.new \
+            filename: ActiveStorage::Filename.new("#{blob.filename.base}.wav"),
+            content_type: "audio/wav",
+            format: :wav
+        end
+    end
 
     delegate :format, to: :specification
 
