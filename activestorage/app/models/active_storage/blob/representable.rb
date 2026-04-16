388901a00e701d5c078464e44aeeb5124e530bd1
iamvanille <vanille.paitier-debray@mail.mcgill.ca>
2026-04-16 12:05:19 -0400

    wav file ext for variants now... check if this breaks anything

--- activestorage/app/models/active_storage/blob/representable.rb
+++ activestorage/app/models/active_storage/blob/representable.rb
@@ -120,7 +120,7 @@ module ActiveStorage::Blob::Representable
       if web_image?
         format || :png
       else
-        :png #THIS IS WHERE IT IS SET but it only works in png locally lol, otherwise i get ArgumentError Exception: wrong number of arguments (given 1, expected 2)
+        :wav #THIS IS WHERE IT IS SET but it only works in png locally lol - actually it doesnt work in png locally either, otherwise i get ArgumentError Exception: wrong number of arguments (given 1, expected 2) (thishappens when a variant must be created. i cannot do it locally)
       end
     end
 
